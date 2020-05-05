80931aaba0e7f7777c3b491db637a2329009191b
Anton Lantukh <alantukh@yandex-team.ru>
2020-05-05 23:23:36 +0300

    hw03-testFramework error -> exception

--- hw03-testFramework/src/main/java/ru/otus/lantukh/tester/AssertionException.java
+++ hw03-testFramework/src/main/java/ru/otus/lantukh/tester/AssertionException.java
@@ -1,6 +1,6 @@
 package ru.otus.lantukh.tester;
 
-public class AssertionException extends Error {
+public class AssertionException extends RuntimeException {
     AssertionException(String message) {
         super(message);
     }
