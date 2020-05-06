2a9c6651da70f8f0d6cdf9b96c3c50737ad35e0b
Anton Lantukh <alantukh@yandex-team.ru>
2020-05-06 11:40:35 +0300

    hw04-gc

---
+++ hw04-gc/src/main/java/ru/otus/lantukh/Animal.java
@@ -0,0 +1,22 @@
+package ru.otus.lantukh;
+
+public class Animal {
+    String sound;
+    String name;
+
+    String getSound() {
+        return sound;
+    }
+
+    String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSound(String sound) {
+        this.sound = sound;
+    }
+}
---
+++ hw04-gc/src/main/java/ru/otus/lantukh/BenchmarkMBean.java
@@ -0,0 +1,7 @@
+package ru.otus.lantukh;
+
+public interface BenchmarkMBean {
+  int getPauseSize();
+
+  void setPauseSize(int size);
+}
