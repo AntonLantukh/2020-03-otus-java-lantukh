0d853df45bb2d81b7168abc4c5f3067763f882bc
Anton Lantukh <alantukh@yandex-team.ru>
2020-11-08 22:24:23 +0300

    hw15-sequence

---
+++ hw15-sequence/src/main/java/ru/otus/lantukh/Main.java
@@ -0,0 +1,52 @@
+package ru.otus.lantukh;
+
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+    private boolean ASC = true;
+    private final Object monitor = new Object();
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        Thread thread1 = new Thread(main::doTask);
+        Thread thread2 = new Thread(main::doTask);
+
+        thread1.start();
+        thread2.start();
+    }
+
+    private int count(int number) {
+        int MAX_INT = 10;
+        int MIN_INT = 1;
+        int returnValue;
+
+        if (number == MAX_INT) {
+            ASC = false;
+        } else if (number == MIN_INT && !ASC) {
+            Thread.currentThread().interrupt();
+        }
+
+        System.out.println(Thread.currentThread().getName() + " | current: " + number);
+        returnValue = ASC ? number + 1 : number - 1;
+
+        return returnValue;
+    }
+
+
+    private void doTask() {
+        int counter = 0;
+
+        synchronized (monitor) {
+            while (!Thread.currentThread().isInterrupted()) {
+                try {
+                    monitor.notify();
+                    Thread.sleep(TimeUnit.SECONDS.toMillis(1));
+                    counter = count(counter);
+                    monitor.wait();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
+    }
+}
